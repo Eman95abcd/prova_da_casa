fde3911f6cdcd70fcea8874f56a1cb360ce41213
Eman95abcd <132665202+Eman95abcd@users.noreply.github.com>
2023-05-13 21:39:51 +0200

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner; // importa la classe Scanner dal package java.util
-
-public class HelloWorld // il nome della classe deve essere uguale al nome del file Java (HelloWorld.java)
-{
-    public static void main(String[] args) // il metodo main è il punto di partenza dell'esecuzione del programma
-    {
-        System.out.println("Hello, world!"); // stampa "Hello, world!" nella console
-    }
-}
